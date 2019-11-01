fa98ab64d9aa9dd4b5a0b0d6e8a11819fcccbea0
Uday Narayan Goel <54022946+uday2000@users.noreply.github.com>
2019-11-01 16:42:51 +0530

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,35 +1,47 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
-    private ImageView playButton = new ImageView();
+    @FXML
+    public ImageView exitButtonImage;
+    @FXML
+    private ImageView playButtonImage = new ImageView();
+    @FXML
+    private Button playButton = new Button();
 
-    private Image playPressed = new Image("file:sprites\\buttons\\playbutton pressed.png");
-    private Image playReleased = new Image("file:sprites\\buttons\\exitbutton.png");
+    private Image playPressed = new Image(getClass().getResourceAsStream("sprites\\buttons\\playbutton pressed.png"));
+    private Image playReleased = new Image(getClass().getResourceAsStream("sprites\\buttons\\playbutton.png"));
+    private Image exitPressed = new Image(getClass().getResourceAsStream("sprites\\buttons\\exitbutton pressed.png"));
+    private Image exitRelease = new Image(getClass().getResourceAsStream("sprites\\buttons\\exitbutton.png"));
+
 
     @FXML
     private void exitAction() {
-        System.out.println(0);
+        exitButtonImage.setImage(exitPressed);
+    }
+
+
+    @FXML
+    private void playAction() throws InterruptedException {
+        playButtonImage.setImage(playPressed);
+    }
+
+    @FXML
+    private void playRelease() {
+        playButtonImage.setImage(playReleased);
+    }
+
+    public void exitGame() {
         System.exit(0);
     }
-    @FXML
-    private void playAction()
-    {
-        System.out.println("hello");
-        playButton.setImage(playPressed);
-        System.out.println("helloagain");
+
+    public void exitReset() {
+        exitButtonImage.setImage(exitRelease);
     }
-
-    @FXML
-    private void playActionRelease()
-    {
-        playButton.setImage(playReleased);
-    }
-
-
-
- }
+}
